f38f66acc7e92c801099eac4afc469a4f935ba29
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-03-17 08:53:35 +0400

    Set up my custom project

--- Jellyfin.Plugin.SubfolderFinder/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.SubfolderFinder/Configuration/PluginConfiguration.cs
@@ -1,6 +1,6 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.Template.Configuration;
+namespace Jellyfin.Plugin.SubfolderFinder.Configuration;
 
 /// <summary>
 /// The configuration options.
--- Jellyfin.Plugin.SubfolderFinder/Plugin.cs
+++ Jellyfin.Plugin.SubfolderFinder/Plugin.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Jellyfin.Plugin.Template.Configuration;
+using Jellyfin.Plugin.SubfolderFinder.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template;
+namespace Jellyfin.Plugin.SubfolderFinder;
 
 /// <summary>
 /// The main plugin.
@@ -26,10 +26,10 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "SubfolderFinder";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("685b7da7-1091-49f9-bfd3-0d703b005490");
 
     /// <summary>
     /// Gets the current plugin instance.
